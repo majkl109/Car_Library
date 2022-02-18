c29eb48e37a8459cefcee4cb6d4a9cdd89e7e076
Y9FYN7724 <Y9FYN7724@LAPTOP-6MIUQFSH>
2022-02-18 05:57:02 +0100

    Add project files.

---
+++ Car_Library/Car_Library.py
@@ -0,0 +1,8 @@
+writer = input("Input manufacturer: ")
+book = input("Input model name: ")
+year = input("Input year of manufacturing: ")
+number_of_pages = input("Input power in HP: ")
+testing = 1234
+
+print(int(year))
+

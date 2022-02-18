4bff7f2eb490fd42b4329c7c1101925e611d5973
Majkl <majkl.andracic@gmail.com>
2022-02-18 14:24:35 +0100

    TODO added

--- Car_Library/Car_Library.py
+++ Car_Library/Car_Library.py
@@ -2,7 +2,8 @@ writer = input("Input manufacturer: ")
 book = input("Input model name: ")
 year = input("Input year of manufacturing: ")
 number_of_pages = input("Input power in HP: ")
-testing = 1234
+
+#TODO create a database so data can be stored in tables and later accessed through GUI
 
 print(int(year))
 
